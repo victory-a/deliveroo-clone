c452b6842cc4f1aff2d9c7959b381651bbeb8090
victory-a <victoryasokomeh@gmail.com>
2023-08-17 23:12:11 +0100

    WIP: restaurant schema

--- sanity/schemas/index.js
+++ sanity/schemas/index.js
@@ -1,6 +1,6 @@
 import blockContent from './blockContent'
 import category from './category'
-import post from './post'
+import post from './restaurant'
 import author from './author'
 
 export const schemaTypes = [post, author, category, blockContent]
